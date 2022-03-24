f38f3800dcfa385945a65fc857ca41e5f8955135
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-24 17:12:07 -0300

    feat: refectory Itens

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Detalhes from "./componentes/Detalhes";
-import Itens from "./componentes/Itens";
 import Topo from "./componentes/Topo";
+
+import Item from "./componentes/Item";
 
 export default function Cesta({ topo, detalhes, itens }) {
   return;
-  <ScrollView>
-    <Topo {...topo} />
-
-    <View style={estilos.cesta}>
-      <Detalhes {...detalhes} />
-
-      <Itens {...itens} />
-    </View>
-  </ScrollView>;
+  <>
+    <FlatList
+      data={itens.lista}
+      renderItem={Item}
+      keyExtractor={({ nome }) => nome}
+      ListHeaderComponent={() => {
+        return;
+        <>
+          <Topo {...topo} />
+          <View style={estilos.cesta}>
+            <Detalhes {...detalhes} />
+          </View>
+        </>;
+      }}
+    />
+  </>;
 }
 
 const estilos = StyleSheet.create({
+  titulo: {
+    color: "#464646",
+    fontWeight: "bold",
+    marginTop: 32,
+    marginBottom20,
+    lineHeight: 32,
+  },
   cesta: {
     paddingVertical: 8,
     paddingHorizontal: 16,
