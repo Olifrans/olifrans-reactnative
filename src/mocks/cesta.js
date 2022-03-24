aa87e9e169e8552f85ab26471b7d74f467a60686
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-24 16:44:10 -0300

    feat: Itens e map.Itens javascript

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,4 +1,8 @@
 import logo from "../../assets/logo.png";
+import tomate from "../../assets/frutas/Tomate.png";
+import brocolis from "../../assets/frutas/Brócolis.png";
+import batata from "../../assets/frutas/Batata.png";
+import abobora from "../../assets/frutas/Abóbora.png";
 
 const cesta = {
   topo: {
@@ -11,7 +15,7 @@ const cesta = {
     nomeFazenda: "Frans Olifrans Farm",
     descricao: "Cesta de produtas especiamente para você e sua familia",
     preco: "R$ 60,00",
-    botao: "Compar"
+    botao: "Compar",
   },
 
   itens: {
